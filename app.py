5aa1c62a730acb78d4a7a83501373ef8af66c1b5
Swapnil Tamrakar <swappy208@gmail.com>
2018-02-03 13:39:42 -0500

    articles

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Articles
 # create an instance of flask
 app = Flask(__name__)
+
+thearticles = Articles()
 
 @app.route('/')
 def index():
@@ -9,6 +12,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = thearticles)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+    return [{
+            'id': 1,
+            'title' : 'Article 1',
+            'body': 'lalalalallalalalala',
+            'createdBy': 'Swapnil Tamrakar',
+            'createdDate': '02-03-2018'
+        },
+        {
+            'id': 2,
+            'title' : 'Article 2',
+            'body': 'the apppappapasdjlk ajsjd kash ks',
+            'createdBy': 'Neil Tam',
+            'createdDate': '02-02-2018'
+        },
+        {
+            'id': 3,
+            'title' : 'Article 3',
+            'body': 'alyriuaytriuyuasiuhfkjhfkjhkjdh jhdsjhfjushdhfdsjfhjdjfjhdhjd hdfsjhjhdhjhfjhasdgjfhgdfjh gdhjgf',
+            'createdBy': 'Disaster Haha',
+            'createdDate': '02-03-2018'
+        }]
