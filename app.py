b00d581a8506ea969f2ce1a77a0195a721260944
Swapnil Tamrakar <swappy208@gmail.com>
2018-02-06 11:46:50 -0500

    User login

--- app.py
+++ app.py
@@ -63,8 +63,48 @@ def register():
 
         flash('You are now registered and can log in', 'success')
 
-        redirect(url_for('index'))
+        return redirect(url_for('index'))
     return render_template('register.html', form = form)
+
+# User Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        #Create Cursor
+        cur = mysql.connection.cursor()
+
+        # User by Username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+            # Compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                app.logger.info('PASSWORD MATCHED')
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error='Invalid Login'
+                app.logger.info('PASSWORD NOT MATCHED')
+                return render_template('login.html', error=error)
+            cur.close()
+        else:
+            app.logger.info('NO USER')
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.secret_key='secret123'
